1f51fd096e6eef08f657bd39d62574307467315c
Leonardo Blautzik <leoblautzik@gmail.com>
2024-03-11 11:34:50 -0300

    Clase 11-03-24

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+
+public class HolaMundo{
+
+  public static void main(String[] args) {
+    System.out.Println("Hola Mundo")
+  }
+}
---
+++ Repaso/src/guiaDeRepaso/Ejercicio08.java
@@ -0,0 +1,18 @@
+package guiaDeRepaso;
+
+public class Ejercicio08 {
+
+	static void mostrarLosNprimerosNaturales (int n) {
+		for(int i = 0; i < n; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void main(String[] args) {
+		mostrarLosNprimerosNaturales(1000);
+
+	}
+	
+	
+
+}
