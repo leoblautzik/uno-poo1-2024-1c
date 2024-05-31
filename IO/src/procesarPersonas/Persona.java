af551cf02330981aa77de6d177bd5f3e6cb17153
Leonardo Blautzik <leoblautzik@gmail.com>
2024-05-31 11:19:22 -0300

    IO Procesar Personas Completo

--- IO/src/procesarPersonas/Persona.java
+++ IO/src/procesarPersonas/Persona.java
@@ -1,5 +1,7 @@
 package procesarPersonas;
 
+import java.util.Objects;
+
 public class Persona implements Comparable<Persona>{
 	
 	private Integer dni;
@@ -34,6 +36,24 @@ public class Persona implements Comparable<Persona>{
 	public int compareTo(Persona p) {
 		return this.getApenom().compareTo(p.getApenom());
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dni);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return this.dni.equals(other.dni);
+	}
+	
 	
 	
 	
